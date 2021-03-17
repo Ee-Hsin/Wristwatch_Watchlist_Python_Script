3c471bf59a124deb8985c9a8bdb2bed53aa98db2
Ee-Hsin <eehsinkok777@gmail.com>
2021-03-18 00:25:01 +0800

    Added fully functioning python script that will convert currency and send emails etc... Added a README for instructions for users to use

---
+++ README.md
@@ -0,0 +1,3 @@
+STEPS to use:
+
+Fill up the form at the beginning of the script, with your User Info and the watches you are looking for.
--- watchcharts_watchlist_scraper.py
+++ watchcharts_watchlist_scraper.py
@@ -1,43 +1,88 @@
+import os
 import requests
 from bs4 import BeautifulSoup
+import smtplib
 
-#TODO: Add Query to URL with the desired watch as a variable, add the minimum and maximum price criteria
+#USERS SHOULD FILL THIS PART UP!
+emailAddress = os.environ.get('emailAddress')
+emailAddressRecipient = os.environ.get('emailAddress')
+gmailAppPassword = os.environ.get('gmailAppPassword')
+#Add or remove watch objects, there is no limit.
+watches = [
+    {'watchName': 'Omega Seamaster 2531.80', 'minPrice' : 900, 'maxPrice': 2700,  'recentListingsLimit': 13},
+    {'watchName': 'Tudor Black Bay 58', 'minPrice' : 900, 'maxPrice': 2700,  'recentListingsLimit': 13},
+    {'watchName': 'Omega Speedmaster', 'minPrice' : 1000, 'maxPrice': 2000,  'recentListingsLimit': 13},
+    ] 
 
-#Don't need to use API for this, no point, I don't need exact prices and watch prices fluctuate much more than forex
+#Don't need to use an API for this, no point honestly, I don't need exact prices and watch prices fluctuate much more than forex
 #honestly
 USD_TO_SGD = 1.35
 EUR_TO_SGD = 1.60
 GBP_TO_SGD = 1.87
 
-URL = "https://watchcharts.com/listings/watch/125?page=1"
+def scrapeWatchCharts(watchName, minPrice, maxPrice, recentListingsLimit):
+    #We want unsold watches from watchcharts
+    URL = "https://watchcharts.com/listings?q={}&page=1&status=unsold".format('%20'.join(watchName.split(' ')))
 
-headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'}
+    #Put your user agent here obviously.
+    headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'}
 
-page = requests.get(URL, headers=headers)
+    #Extracting prices
+    page = requests.get(URL, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    elements = soup.find_all("h4",{"class" : "m-0"})
+    #Putting them in an array
+    prices = []
+    for element in elements:
+        prices.append(element.get_text().strip())
 
-soup = BeautifulSoup(page.content, 'html.parser')
+    count = 0
+    for price in prices:
+        if price[0] == '$':
+            convertedPrice = int(price[1:].replace(',', '')) * USD_TO_SGD
+        elif price[0] == '€':
+            convertedPrice = int(price[1:].replace(',', '')) * EUR_TO_SGD
+        elif price[0] == '£':
+            convertedPrice = int(price[1:].replace(',', '')) * GBP_TO_SGD
 
-elements = soup.find_all("h4",{"class" : "m-0"})
+        #rounding one more time after forex conversion
+        convertedPrice = round(convertedPrice)
 
-prices = []
+        #If smaller than desired price
+        if(convertedPrice < maxPrice and convertedPrice > minPrice):
+            send_mail(watchName, maxPrice, URL)
+            break #break at the end of it, if one of the watches fits the criteria, we send a notification alerting the user
+            #to check out the search page themselves. There the user can find the cheapest watch, if there is a cheaper one
+            #than the one we alerted on.
 
-for element in elements:
-    prices.append(element.get_text().strip())
+        #Exit after we go through the limit of most recent listings we want.
+        count += 1
+        if (count >= recentListingsLimit):
+            break
 
-for price in prices:
-    if price[0] == '$':
-        convertedPrice = int(price[1:].replace(',', '')) * USD_TO_SGD
-    elif price[0] == '€':
-        convertedPrice = int(price[1:].replace(',', '')) * EUR_TO_SGD
-    elif price[0] == '£':
-        convertedPrice = int(price[1:].replace(',', '')) * GBP_TO_SGD
+def send_mail(watchName, maxPrice, URL):
+    global emailAddress, emailAddressRecipient, gmailAppPassword
 
-    convertedPrice = round(convertedPrice)
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
 
-    #If smaller than desired price
-    #if():
-        #send email
+    server.login(emailAddress, gmailAppPassword)
 
+    subject = 'The price of {} has fallen below {}'.format(watchName, maxPrice)
+    body = "There has been a listing below your ideal price, so check out the URL QUICK URL: {}".format(URL)
+    msg = f"Subject:{subject}\n\n{body}"
 
+    server.sendmail(
+        emailAddress,
+        emailAddressRecipient,
+        msg
+    )
+    print("EMAIL HAS BEEN SENT!")
 
+def searchForWatches():
+    for watch in watches:
+        scrapeWatchCharts(watch['watchName'],watch['minPrice'],watch['maxPrice'],watch['recentListingsLimit'])
 
+searchForWatches()
